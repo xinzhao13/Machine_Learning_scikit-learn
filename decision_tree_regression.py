eec7d5230df73fefc802580941d293347851bd7e
Nicolas Essipova <31189442+NicoEssi@users.noreply.github.com>
2019-02-15 18:52:19 +0100

    Add files via upload

---
+++ decision_tree_regression.py
@@ -0,0 +1,24 @@
+# Importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Initializing and fitting the regressor to dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor()
+regressor.fit(X, y)
+
+# Visualisation
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'green')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
